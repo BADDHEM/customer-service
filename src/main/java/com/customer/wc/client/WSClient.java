0c0cd3ddd6f1d57218b3e1b89fe6f06d92d016f1
BADDHEM <baddhemvenki@gmail.com>
2019-02-21 23:39:26 +0530

    addedheader

--- src/main/java/com/customer/wc/client/WSClient.java
+++ src/main/java/com/customer/wc/client/WSClient.java
@@ -5,7 +5,14 @@ import java.io.IOException;
 import javax.xml.soap.MessageFactory;
 import javax.xml.soap.SOAPConstants;
 import javax.xml.soap.SOAPException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamSource;
 
+import org.apache.cxf.binding.soap.SoapHeader;
+import org.apache.cxf.binding.soap.SoapMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.WebServiceMessage;
@@ -14,6 +21,8 @@ import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
 import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
 import org.springframework.ws.transport.http.HttpComponentsMessageSender;
+
+import com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl;
 
 public class WSClient extends WebServiceGatewaySupport {
 
@@ -28,8 +37,10 @@ public class WSClient extends WebServiceGatewaySupport {
 	   HttpComponentsMessageSender sender = new HttpComponentsMessageSender();
 	   sender.setConnectionTimeout(600*1000);
 	   sender.setReadTimeout(600*1000);
+	   template.setMessageSender(sender);
 	   MessageFactory factory = MessageFactory.newInstance(SOAPConstants.SOAP_1_2_PROTOCOL);
 	   SaajSoapMessageFactory saajSoapMessageFactory = new SaajSoapMessageFactory(factory);
+	   template.setMessageFactory(saajSoapMessageFactory);
 	   marshaller.setMtomEnabled(true);
 	   template.setMarshaller(marshaller);
 	   template.setUnmarshaller(unMarshaller);
@@ -43,5 +54,18 @@ public class WSClient extends WebServiceGatewaySupport {
 		   
 	   });
 	   
+   }
+   
+   public void setHeader(WebServiceMessage message) throws TransformerConfigurationException {
+	   SoapMessage soapEnvelop = (SoapMessage) message;
+	   StreamSource source = new StreamSource("header");
+	   TransformerFactoryImpl factory = new TransformerFactoryImpl();
+	   Transformer transformer = factory.newTransformer();
+	   try {
+		transformer.transform(source,null );
+	} catch (TransformerException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
    }
 }
